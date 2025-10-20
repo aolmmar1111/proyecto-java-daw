0fa8ed021559d8e14bb48f826a80179e398ca8a4
aolmmar1111 <alejandro.delolmo1111@gmail.com>
2025-10-20 09:46:04 +0200

    Commit en origin/holamundo

--- src/main/java/alboran/HolaMundo.java
+++ src/main/java/alboran/HolaMundo.java
@@ -3,5 +3,6 @@ package alboran;
 public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("¡Hola, Mundo! Este es mi primer programa versionado con Git");
+        System.out.println("Prueba holamundo");
     }
 }
