3d27eaf8c3decfeac8963fe7c3193b409a4c977b
aolmmar1111 <alejandro.delolmo1111@gmail.com>
2025-10-20 09:35:30 +0200

    Cambio archivo en rama origin/holamundo

--- src/main/java/alboran/HolaMundo.java
+++ src/main/java/alboran/HolaMundo.java
@@ -3,7 +3,5 @@ package alboran;
 public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("¡Hola, Mundo! Este es mi primer programa versionado con Git");
-
-        System.out.println("La gestión de versiones es una competencia clave.");
     }
 }
